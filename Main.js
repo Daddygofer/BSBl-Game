63fd824731b1752245a70ecdb3218cf9875ed6ac
Daddygofer <61069691+Daddygofer@users.noreply.github.com>
2020-05-01 15:44:21 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,20 +2,39 @@
 var hits = 0;
 var outs = 0;
 var runs = 0;
-
+document.getElementById("hit").innerHTML = 'hits: ' + hits;
+document.getElementById("out").innerHTML = 'outs: ' + outs;
+document.getElementById("run").innerHTML = 'runs: ' + runs;
 //chance
     function startgame() {
       var outcome = Math.floor(Math.random() * 3)
       document.getElementById("button-text").innerHTML = outcome;
-     if (outcome < 1){
+       //activation
+     document.getElementById("hit").innerHTML = 'hits: ' + hits;
+     document.getElementById("out").innerHTML = 'outs: ' + outs;
+     document.getElementById("run").innerHTML = 'runs: ' + runs;
+    if (outcome === 2){
     hits = hits + 1;
-    };
-    if (outcome < 1) {
+    document.getElementById("button-text").innerHTML = "HIT";
+      if (hits >= 3) {
+        runs = runs + 1   
+      }
+    }
+    if (outcome < 2) {
     outs = outs+ 1;
-    } 
- }
- 
- //activation
-document.getElementById("hit").innerHTML = 'hits: ' + hits;
-document.getElementById("out").innerHTML = 'outs: ' + outs;
-document.getElementById("run").innerHTML = 'runs: ' + runs;
+    document.getElementById("button-text").innerHTML = "Out!";
+      if (outs === 3) {
+        hits = hits * 0;
+        outs = hits * 0; 
+        runs = runs * 0;
+        document.getElementById("hit").innerHTML = 'hits: ' + hits;
+        document.getElementById("out").innerHTML = 'outs: ' + outs;
+        document.getElementById("run").innerHTML = 'runs: ' + runs;
+      if(outs === 0){
+        document.getElementById("button-text").innerHTML = "New Inning"
+      }
+    }
+  }
+}
+
+
